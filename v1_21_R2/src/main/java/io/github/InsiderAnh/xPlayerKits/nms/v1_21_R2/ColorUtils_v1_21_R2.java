52900860cae67dc29f356adac57694477dbcf3ba
InsiderAnh <leo_yt_29@hotmail.com>
2025-08-13 18:49:25 -0500

    Added all 1.20 and 1.21

---
+++ v1_21_R2/src/main/java/io/github/InsiderAnh/xPlayerKits/nms/v1_21_R2/ColorUtils_v1_21_R2.java
@@ -0,0 +1,39 @@
+package io.github.InsiderAnh.xPlayerKits.nms.v1_21_R2;
+
+import io.github.InsiderAnh.xPlayerKits.api.ColorUtils;
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class ColorUtils_v1_21_R2 extends ColorUtils {
+
+    private final Pattern pattern = Pattern.compile("&?#[a-fA-F0-9]{6}");
+
+    public List<String> color(List<String> message) {
+        return message.stream().map(this::color).collect(Collectors.toList());
+    }
+
+    public String color(String message) {
+        return translateAlternateColorCodes('&', message);
+    }
+
+    public String translateAlternateColorCodes(char altColorChar, String message) {
+        Matcher matcher = pattern.matcher(message);
+        StringBuffer stringBuffer = new StringBuffer();
+        while (matcher.find()) {
+            String color = message.substring(matcher.start(), matcher.end());
+            try {
+                ChatColor chatColor = ChatColor.of(color.replace("&", ""));
+                matcher.appendReplacement(stringBuffer, chatColor.toString());
+            } catch (Exception ignored) {
+            }
+        }
+        matcher.appendTail(stringBuffer);
+        message = stringBuffer.toString();
+        return ChatColor.translateAlternateColorCodes(altColorChar, message);
+    }
+
+}
