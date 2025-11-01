ed48fddae9a588794c2d2264357f4cb9ff85795f
InsiderAnh <leo_yt_29@hotmail.com>
2025-11-01 18:27:03 -0500

    bump: version

--- Common/src/main/java/io/github/InsiderAnh/xPlayerKits/bstats/MetricsLite.java
+++ Common/src/main/java/io/github/InsiderAnh/xPlayerKits/bstats/MetricsLite.java
@@ -64,7 +64,7 @@ public class MetricsLite {
             }
         }
         // Load the data
-        boolean enabled = true;
+        boolean enabled = config.getBoolean("enabled", true);
         String serverUUID = config.getString("serverUuid");
         boolean logErrors = config.getBoolean("logFailedRequests", false);
         boolean logSentData = config.getBoolean("logSentData", false);
